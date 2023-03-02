ad06e1d64a461a23069af3d7e4a976d34393a11f
Lucoero <120726582+Lucoero@users.noreply.github.com>
2023-03-02 21:11:51 +0100

    .

---
+++ Mas sobre Arrays/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Mas_sobre_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] myArray = {1, 2};
+            int[,] myArray2 = { 3, 4 };
+            int[,] myArray3 = new Array;
+            myArray.CopyTo(myArray3);
+
+        }
+    }
+}
