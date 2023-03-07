f61179f29e20d35f8e78f4f27a8f1abb67081997
Lucoero <120726582+Lucoero@users.noreply.github.com>
2023-03-07 16:30:34 +0100

    v0.2

---
+++ Funciones_asincronas/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Funciones_asincronas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static async bool Wait(int seconds)
+            {
+                await Task.Delay(seconds);
+                return true; 
+            }
+            Console.Write("Dime cuantos segundos quieres esperar: ");
+            try
+            {
+                Wait(Convert.ToInt32(Console.ReadLine()));
+                Console.WriteLine("Listo!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            
+        }
+    }
+}
